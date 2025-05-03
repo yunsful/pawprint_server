a0a540eb270f9e733e0430193dc300bdfe6a0fff
yunsful <bailey05280@naver.com>
2025-05-04 00:44:17 +0900

    feat: 회원 엔티티 추가

---
+++ src/main/java/pawprint/demo/domain/Member.java
@@ -0,0 +1,24 @@
+package pawprint.demo.domain;
+
+import jakarta.persistence.*;
+import lombok.*;
+import pawprint.demo.domain.base.BaseEntity;
+
+@Entity
+@Builder
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+@Getter
+public class Member extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    
+    private String name;
+    private String userId;
+    private String password;
+    private String statusNote;
+    private String profile;
+    
+}
