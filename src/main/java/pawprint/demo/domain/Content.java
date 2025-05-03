dd1a7012623f22426f6029b611630c814bcf17c0
yunsful <bailey05280@naver.com>
2025-05-04 00:43:11 +0900

    feat: 게시글 엔티티 추가

---
+++ src/main/java/pawprint/demo/domain/Content.java
@@ -0,0 +1,27 @@
+package pawprint.demo.domain;
+
+import jakarta.persistence.*;
+import lombok.*;
+import pawprint.demo.domain.base.BaseEntity;
+
+import static jakarta.persistence.FetchType.*;
+
+@Entity
+@Builder
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+@Getter
+public class Content extends BaseEntity {
+    
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    
+    private String body;
+    private Integer likesCount;
+    private Integer commentsCount;
+    
+    @ManyToOne(fetch = LAZY)
+    @JoinColumn(name = "member_id")
+    private Member member;
+}
