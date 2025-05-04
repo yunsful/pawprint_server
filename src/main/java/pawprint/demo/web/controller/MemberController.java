0956ef833819db843b893a06ccc894389915594e
yunsful <bailey05280@naver.com>
2025-05-04 15:41:50 +0900

    feat: 회원가입 API 구현

--- src/main/java/pawprint/demo/web/controller/MemberController.java
+++ src/main/java/pawprint/demo/web/controller/MemberController.java
@@ -6,6 +6,8 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import pawprint.demo.apiPayload.ApiResponse;
+import pawprint.demo.domain.Member;
+import pawprint.demo.service.MemberService;
 import pawprint.demo.web.dto.MemberRequest;
 import pawprint.demo.web.dto.MemberResponse;
 
@@ -14,12 +16,16 @@ import pawprint.demo.web.dto.MemberResponse;
 @RequestMapping("/api/users")
 public class MemberController {
     
+    private final MemberService memberService;
+    
     @PostMapping("/join")
     public ApiResponse<MemberResponse.MemberIdDto> join(MemberRequest.MemberJoinDto joinDto) {
         
+        Member savedMember = memberService.join(joinDto);
+        
         return ApiResponse.onSuccess(
                 MemberResponse.MemberIdDto.builder()
-                        .id(1L)
+                        .id(savedMember.getId())
                         .build());
     }
 }
