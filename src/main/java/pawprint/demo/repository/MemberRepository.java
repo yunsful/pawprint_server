c93fc128d54a7ab857e675470af4fd634d6521b9
yunsful <bailey05280@naver.com>
2025-05-04 15:43:05 +0900

    feat: 회원아이디 중복 확인 메서드 구현

--- src/main/java/pawprint/demo/repository/MemberRepository.java
+++ src/main/java/pawprint/demo/repository/MemberRepository.java
@@ -4,6 +4,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import pawprint.demo.domain.Member;
 
 public interface MemberRepository extends JpaRepository<Member, Long> {
-
-
+    
+    
+    Boolean existsByUserId(String userId);
 }
