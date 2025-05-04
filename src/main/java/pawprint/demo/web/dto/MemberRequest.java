d58653abbbc987b97401f070e061a02c96475ea5
yunsful <bailey05280@naver.com>
2025-05-04 15:00:37 +0900

    feat: 회원가입 요청 dto 기본 틀 작성

--- src/main/java/pawprint/demo/web/dto/MemberRequest.java
+++ src/main/java/pawprint/demo/web/dto/MemberRequest.java
@@ -1,4 +1,15 @@
 package pawprint.demo.web.dto;
 
+import lombok.Getter;
+
 public class MemberRequest {
+    
+    @Getter
+    public static class MemberJoinDto {
+        
+        private String userId;
+        private String password;
+        private String name;
+        private String statusNote;
+    }
 }
