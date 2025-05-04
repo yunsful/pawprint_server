5d1d3a22822182cf1c969eb78757231ae0845972
yunsful <bailey05280@naver.com>
2025-05-04 15:09:21 +0900

    feat: 시큐리티 설정 클래스 기본 틀 작성

---
+++ src/main/java/pawprint/demo/config/SecurityConfig.java
@@ -0,0 +1,42 @@
+package pawprint.demo.config;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+@RequiredArgsConstructor
+public class SecurityConfig {
+    
+    @Bean
+    public BCryptPasswordEncoder bCryptPasswordEncoder() {
+        
+        return new BCryptPasswordEncoder();
+    }
+    
+    @Bean
+    SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        
+        http
+                .csrf(auth -> auth.disable())
+                .formLogin((auth) -> auth.disable())
+                .httpBasic((auth) -> auth.disable())
+                
+                .authorizeHttpRequests((auth) -> auth
+                        .requestMatchers("/login", "/", "/join").permitAll()
+                        .requestMatchers("/admin").hasRole("ADMIN")
+                        .anyRequest().authenticated())
+                
+                .sessionManagement((session) -> session
+                        .sessionCreationPolicy(SessionCreationPolicy.STATELESS));
+        
+        return http.build();
+    }
+
+}
